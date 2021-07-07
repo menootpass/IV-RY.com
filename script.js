9b02c689b592a9f679a2734cb5066ed212faf1ba
menootpass <mrs.farhanary@gmail.com>
2021-07-07 10:34:25 +0700

    responsive web for fiver

---
+++ script.js
@@ -0,0 +1,6 @@
+function readmore() {
+  var readmore = document.getElementById("readmore");
+  readmore.classList.toggle("active");
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+}
